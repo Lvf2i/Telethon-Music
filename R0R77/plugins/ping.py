a4e7a003e442559dce090c858131217cd396cbc1
Lvf2i <91951563+Lvf2i@users.noreply.github.com>
2022-08-12 16:56:55 +0300

    Update ping.py

--- R0R77/plugins/ping.py
+++ R0R77/plugins/ping.py
@@ -5,7 +5,7 @@ from telethon import Button, events
 from R0R77 import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/2ad68bd0e391a69163d0a.jpg"
+    "PING_PIC", "https://telegra.ph/file/8a2b1deadab4f9e15b001.jpg"
 )
 ms = 4
 
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
 
 @R0R77.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    UMM = [[Button.url("السورس", "https://t.me/JMTHON")]]
+    UMM = [[Button.url("السورس", "https://t.me/meeddu")]]
     await R0R77.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
