968598aaa3bca43ecf8ffd6f8a669527878b3dca
Lvf2i <91951563+Lvf2i@users.noreply.github.com>
2023-03-20 19:50:21 +0300

    Update help.py

--- R0R77/plugins/help.py
+++ R0R77/plugins/help.py
@@ -8,7 +8,7 @@ btn =[
     [Button.inline("القفل", data="locks"), Button.inline("اخرى", data="misc")],
     [Button.inline("القائمة الرئيسية", data="start")]]
 
-HELP_TEXT = "اهلا بك في قائمة اوامر سورس جمثون\n\nاضغط على الازرار من الاسفل:"
+HELP_TEXT = "اهلا بك في قائمة اوامر سورس الغفور\n\nاضغط على الازرار من الاسفل:"
 
 
 @R0R77.on(events.NewMessage(pattern="[!?/]الاوامر"))
